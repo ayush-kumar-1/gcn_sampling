083a84c3a5668a21e51d4e94191cb3549a116719
ayush-kumar-1 <58648531+ayush-kumar-1@users.noreply.github.com>
2021-12-02 13:32:54 -0500

    Fixed the mistake

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 def main(): 
     print("This program converts Celsius to Fahrenheit.")
     celsius = eval(input("What is the Celsius temperature? "))
-    fahrenheit = 2 * celsius + 30
+    fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
 
 if __name__ == '__main__':
